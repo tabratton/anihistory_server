6e72fa5553931e6dda4a164cbfd84591cca2e5cf
Tyler Bratton <tylerbratton96@gmail.com>
2018-10-03 20:37:54 -0400

    Add license file

--- src/schema.rs
+++ src/schema.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018, Tyler Bratton
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 table! {
     anime (anime_id) {
         anime_id -> Int4,
